d6ec21efd56d94f6380f7e23ea7cd960c52861f1
avih <avinoam.hershler@mail.huji.ac.il>
2019-11-19 22:21:50 +0200

    Imp'd Parser

--- Parser.py
+++ Parser.py
@@ -1,3 +1,8 @@
+import re
+
+from ASMCode import ASMCode
+
+
 class Parser:
 
     def __init__(self, filename):
@@ -5,11 +10,35 @@ class Parser:
         Create a parser object over a .asm file
         :param filename: Path (absolute or relative) to the .asm file.
         """
-        # TODO: implement.
+        self.__filename = filename
 
     def parse(self):
         """
         Parse the .asm file, returning an object of type ASMCode that is ready for compilation.
         :return: An ASMCode object for the parsed code, including symbol values if there are any.
         """
-        # TODO: implement.
+        lines = []
+        symbols = dict()
+        with open(self.__filename, 'r') as file:
+            line = file.readline()
+            line_index = 0
+            while line:
+                line = line.replace('\s', '')
+                line = line[:line.find('//')]
+
+                if not line:
+                    continue
+
+                if line.startswith('('):
+                    pattern = re.compile('\((.+)\)')
+                    result = re.match(pattern, line)
+                    label = result.group(0)
+                    symbols[label] = line_index
+
+                else:
+                    lines.append(line)
+                    line_index += 1
+
+                line = file.readline()
+
+        return ASMCode(lines, symbols)
