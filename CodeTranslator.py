5adbb3bca76785d59b72fe4d2f7132562e2fb55f
Avinoam <avinoam@hershler.com>
2019-11-28 19:58:52 +0200

    first commit

--- CodeTranslator.py
+++ CodeTranslator.py
@@ -1,76 +1,28 @@
-DEST_DICT = {'': '000', 'M': '001', 'D': '010', 'MD': '011', 'A': '100', 'AM': '101', 'AD': '110',
-             'AMD': '111'}
-COMP_DICT = {'0': '110101010', '1': '110111111', '-1': '110111010', 'D': '110001100', 'A': '110110000',
-             '!D': '110001101', '!A': '110110001', '-D': '110001111', '-A': '110110011', 'D+1': '110011111',
-             'A+1': '110110111', 'D-1': '110001110', 'A-1': '110110010', 'D+A': '110000010',
-             'D-A': '110010011',
-             'A-D': '110000111', 'D&A': '110000000', 'D|A': '110010101', 'M': '111110000', '!M': '111110001',
-             '-M': '111110011', 'M+1': '111110111', 'M-1': '111110010', 'D+M': '111000010',
-             'D-M': '111010011',
-             'M-D': '111000111', 'D&M': '111000000', 'D|M': '111010101', 'D<<': '010110000',
-             'A<<': '010100000',
-             'M<<': '011100000', 'D>>': '010010000', 'A>>': '010000000', 'M>>': '011000000'}
-JMP_DICT = {'': '000', 'JGT': '001', 'JEQ': '010', 'JGE': '011', 'JLT': '100', 'JNE': '101', 'JLE': '110',
-            'JMP': '111'}
+
 
 
 class CodeTranslator:
     """
-    Represents an object that can translate ASMCode objects into binary HACK machine code.
+    Represents an object that can translate VMCode objects into hack assembly code.
     """
 
-    def __init__(self, parsed_asm_code):
+    def __init__(self, parsed_VM_code):
         """
-        Create a translator object that can translate a specific ASMCode object into HACK machine language.
-        :param parsed_asm_code: An ASMCode object.
+        Create a translator object that can translate a specific VMCode object into HACK assembly code.
+        :param parsed_VM_code: An VMCode object.
         """
-        self.__asm_code = parsed_asm_code
+        self.__VM_code = parsed_VM_code
 
     def translate(self):
         """
-        Translate the asm code into HACK machine code.
+        Translate the VM code into HACK machine code.
         :return: A 16-bit byte array representing machine code for HACK.
         """
-        lst = []
-        for line in self.__asm_code.get_asm_code():
-            if line[0] == '@':
-                instruction = self.__parse_a_instruction(line)
-            else:
-                instruction = self.__parse_c_instruction(line)
-            lst.append(instruction)
-        return lst
 
-    def __parse_a_instruction(self, instruction):
-        address = instruction[1:]
+    def get_instruction_type(self, line):
+        """
+        Recieves a line of vm code and determines the type of the command.
+        :param line: line of code to determine type.
+        :return: the type of instruction.
+        """
 
-        if address.isnumeric():
-            address = bin(int(address))[2:]
-        else:
-            address = self.__asm_code.get_symbol_value(address)
-            address = bin(int(address))[2:]
-        return address.zfill(16)
-
-    def __parse_c_instruction(self, instruction):
-        split_index = instruction.find(';')
-        jump = ''
-        comp = instruction
-        if split_index >= 0:
-            comp = instruction[:split_index]
-            jump = instruction[split_index + 1:]
-
-        split_index = comp.find('=')
-        dest = ""
-        if split_index >= 0:
-            dest = comp[:split_index]
-            comp_cmd = comp[split_index + 1:]
-        else:
-            comp_cmd = comp
-
-        dest = DEST_DICT[dest]
-        comp_cmd = COMP_DICT[comp_cmd]
-
-        jump = JMP_DICT[jump]
-
-        instruction_bits = '1' + comp_cmd + dest + jump
-
-        return instruction_bits
--- Parser.py
+++ Parser.py
@@ -1,31 +1,29 @@
 import re
 
-from ASMCode import ASMCode
+from VMCode import VMCode
 
 
 class Parser:
 
     def __init__(self, filename):
         """
-        Create a parser object over a .asm file
-        :param filename: Path (absolute or relative) to the .asm file.
+        Create a parser object over a .vm file
+        :param filename: Path (absolute or relative) to the .vm file.
         """
         self.__filename = filename
 
     def parse(self):
         """
-        Parse the .asm file, returning an object of type ASMCode that is ready for compilation.
-        :return: An ASMCode object for the parsed code, including symbol values if there are any.
+        Parse the .vm file, returning an object of type VMCode that is ready for compilation.
+        :return: An VMCode object for the parsed code, including symbol values if there are any.
         """
         lines = []
-        symbols = dict()
         with open(self.__filename, 'r') as file:
             line = file.readline()
-            line_index = 0
             while line:
 
-                # Erase all white spaces:
-                line = re.sub('\s', '', line)
+                # Erase all leading and trailing white spaces:
+                line = line.strip()
 
                 # Find and erase comments:
                 comment = line.find('//')
@@ -36,18 +34,16 @@ class Parser:
                     line = file.readline()
                     continue
 
-                # Find and add label to label dictionary:
-                if line.startswith('('):
-                    pattern = re.compile('\((.+)\)')
-                    result = re.match(pattern, line)
-                    label = result.group(1)
-                    symbols[label] = line_index
+                # # Find and add label to label dictionary:
+                # if line.startswith('('):
+                #     pattern = re.compile('\((.+)\)')
+                #     result = re.match(pattern, line)
+                #     label = result.group(1)
+                #     symbols[label] = line_index
 
                 # Regular instruction - add to list of instructions:
-                else:
-                    lines.append(line)
-                    line_index += 1
 
+                lines.append(line)
                 line = file.readline()
 
-        return ASMCode(lines, symbols)
+        return VMCode(lines, file)
